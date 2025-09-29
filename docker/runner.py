4bfe46ed0650cb00d1b827c778b2e19f0775df88
JP Swinski <jp.swinski@nasa.gov>
2025-09-29 18:16:16 +0000

    updates to atl24d runner

--- docker/runner.py
+++ docker/runner.py
@@ -29,16 +29,130 @@
 
 import sys
 import json
+from h5coro import h5coro, s3driver
+import numpy as np
 import pandas as pd
+
+# ########################################################
+# Constants
+# ########################################################
+
+BEAMS = [ "gt1l", "gt1r", "gt2l", "gt2r", "gt3l", "gt3r" ]
+
+MONTH_TO_SEASON = { #[is_north][month] --> 0: winter, 1: spring, 2: summer, 3: fall
+    True: { 1: 0, 2: 0, 3: 0, 4: 1, 5: 1, 6: 1, 7: 2, 8: 2, 9: 2, 10: 3, 11: 3, 12: 3 },
+    False: { 1: 2, 2: 2, 3: 2, 4: 3, 5: 3, 6: 3, 7: 0, 8: 0, 9: 0, 10: 1, 11: 1, 12: 1 }
+}
 
 # ########################################################
 # Metadata
 # ########################################################
 
+# constant - statistics to generate for granule
+METADATA_STATS = [
+    ('granule',''),
+    ('beam',''),
+    ('region',''),
+    ('season',''),
+    ('total_photons',''),
+    ('subaqueous_photons',''),
+    ('sea_surface_photons',''),
+    ('sea_surface_std','.3f'),
+    ('bathy_photons',''),
+    ('bathy_mean_depth','.3f'),
+    ('bathy_min_depth','.3f'),
+    ('bathy_max_depth','.3f'),
+    ('bathy_std_depth','.3f'),
+    ('bathy_above_sea_surface', ''),
+    ('bathy_below_sensor_depth', ''),
+    ('histogram',''),
+    ('polygon', ''),
+    ('begin_time', ''),
+    ('end_time', '')
+]
+
+# constant - variables to read from granule
+METADATA_VARIABLES = [
+    "ortho_h",
+    "surface_h",
+    "class_ph"
+]
+
+# function - processing granule
 def metadata(parms):
+
+    # initialize arguments
     granule = parms["granule"]
-    print(f'Generating metadata for {granule}')
-    return {"granule": granule}, True
+    path = parms["url"].split("s3://")[-1] + "/" + granule
+    csv_lines = ""
+
+    try:
+        # open granule
+        h5obj = h5coro.H5Coro(path, s3driver.S3Driver, errorChecking=True, verbose=False, credentials={"role": True, "role":"iam"}, multiProcess=False)
+
+        # read datasets
+        datasets = [f'{beam}/{var}' for beam in BEAMS for var in METADATA_VARIABLES]
+        promise = h5obj.readDatasets(datasets + ['metadata/extent'], block=True, enableAttributes=False)
+
+        # pull out metadata
+        extent = json.loads(promise["metadata/extent"])
+
+        # process each beam in the granule
+        for beam in BEAMS:
+            try:
+                # get info
+                month = int(granule[10:12])
+                region = int(granule[27:29])
+
+                # read columns
+                columns = {var: promise[f'{beam}/{var}'][:] for var in METADATA_VARIABLES}
+
+                # build dataframes
+                df = pd.DataFrame(columns)
+                df["depth"] = df["surface_h"] - df["ortho_h"]
+                df_sea_surface = df[df["class_ph"] == 41]
+                df_subaqueous = df[df["class_ph"].isin([0, 40]) & df["depth"] >= 0.0] # treating depth == 0.0 as subaqueous to be consistent with histogram below
+                df_bathy = df[df["class_ph"] == 40]
+
+                # build histogram
+                values = df_bathy["depth"].to_numpy()
+                hist, bin_edges = np.histogram(values, bins=np.arange(0, 51, 1)) # [0.0, 1.0), [1.0, 2.0), ..., [49.0, 50.0], note last range is fully closed
+                hist_str = ' '.join([f"{int(value)}" for value in hist])
+
+                # build stats
+                row = {
+                    "granule": granule,
+                    "beam": beam,
+                    "region": region,
+                    "season": MONTH_TO_SEASON[region<8][month],
+                    "total_photons": len(df),
+                    "subaqueous_photons": len(df_subaqueous),
+                    "sea_surface_photons": len(df_sea_surface),
+                    "sea_surface_std": df_sea_surface["ortho_h"].std(),
+                    "bathy_photons": len(df_bathy),
+                    "bathy_mean_depth": df_bathy["depth"].mean(),
+                    "bathy_min_depth": df_bathy["depth"].min(),
+                    "bathy_max_depth": df_bathy["depth"].max(),
+                    "bathy_std_depth": df_bathy["depth"].std(),
+                    "bathy_above_sea_surface": len(df_bathy[df_bathy["depth"] < 0.0]),
+                    "bathy_below_sensor_depth": len(df_bathy[df_bathy["depth"] > 50.0]),
+                    "histogram": hist_str,
+                    "polygon": extent["polygon"],
+                    "begin_time": extent["begin_time"],
+                    "end_time": extent["end_time"]
+                }
+
+                # build csv line
+                csv_lines += ','.join([f'{row[entry[0]]:{entry[1]}}' for entry in METADATA_STATS]) + "\n"
+
+            except TypeError:
+                pass # missing beam
+
+        # return csv lines
+        return csv_lines, True
+
+    except Exception as e:
+        return f'{granule} - {e}', False
 
 # ########################################################
 # Main
