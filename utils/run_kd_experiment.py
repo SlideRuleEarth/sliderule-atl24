ef9061a6d845696863bda569f5aa4caca060a0d9
JP Swinski <jp.swinski@nasa.gov>
2026-05-28 14:39:23 +0000

    reworked processing flow for kd experiment to build atl09 granules into cycle file

--- utils/run_kd_experiment.py
+++ utils/run_kd_experiment.py
@@ -8,12 +8,12 @@ from sliderule import sliderule, earthdata
 parser = argparse.ArgumentParser(description="""Kd Experiment""")
 parser.add_argument('--name',       type=str,               default="kd_experiment")
 parser.add_argument('--script',     type=str,               default="utils/kd_experiment.lua")
-parser.add_argument('--granule',    type=str,               default=None) # "ATL03_20241107234251_08052501_007_01.h5"
-parser.add_argument('--granules',   type=str,               default=None) # "data/atl03_granules_cycle_1.txt"
+parser.add_argument('--arg',        type=str,               default=None) # ATL03_20241107234251_08052501_007_01.h5,ATL09_20241107234251_08052501_007_01.h5
+parser.add_argument('--args',       type=str,               default=None) # data/atl03_granules_cycle_1.txt
 parser.add_argument('--submission', type=str,               default="data/kd_experiment_submission.txt")
 parser.add_argument('--outputs',    type=str,               default="data/kd_experiment_outputs.txt")
 parser.add_argument('--cycle',      type=int,               default=None) # 1
-parser.add_argument('--run',        type=str,               default=None) # "run url"
+parser.add_argument('--results',    type=str,               default=None) # "run url"
 parser.add_argument('--submit',     action='store_true',    default=False)
 parser.add_argument('--status',     action='store_true',    default=False)
 args,_ = parser.parse_known_args()
@@ -25,28 +25,16 @@ session.authenticate() # gives privileges to access SlideRule Runner
 # submit jobs
 if args.submit:
     # get list of granules
-    if args.granule:
-        granules = [args.granule]
-    elif args.granules:
-        granules = []
-        with open(args.granules, "r") as file:
-            for granule in file.readlines():
-                granules.append(granule.strip())
+    if args.arg:
+        args_list = [args.arg]
+    elif args.args:
+        args_list = []
+        with open(args.args, "r") as file:
+            for arg in file.readlines():
+                args_list.append(arg.strip())
     else:
-        print("Error: must supply granules to process")
+        print("Error: must supply arguments to process")
         sys.exit(1)
-    # append ATL09 granule to each argument
-    args_list = []
-    for granule in granules:
-        rgt = int(granule[21:25])
-        cycle = int(granule[25:27])
-        name_filter = f'*_{rgt:04d}{cycle:02d}??_*'
-        atl09_parms = {
-            "asset": "icesat2-atl09",
-            "name_filter": name_filter
-        }
-        granule09 = earthdata.search(atl09_parms)
-        args_list.append(f"{granule},{granule09[0]}")
     # submit jobs to runner
     print(f"Submitting job {args.name} using script {args.script} with {len(args_list)} entries")
     lua_script = open(args.script, "r").read()
@@ -57,17 +45,33 @@ if args.submit:
 
 # create cycle file
 if args.cycle:
+    args_list = []
     parms = {
         "asset": "icesat2",
         "cycle": args.cycle,
         "max_resources": 100000
     }
     granules = sliderule.source("earthdata", parms)
+    for granule in granules:
+        rgt = int(granule[21:25])
+        cycle = int(granule[25:27])
+        name_filter = f'*_{rgt:04d}{cycle:02d}??_*'
+        atl09_parms = {
+            "asset": "icesat2-atl09",
+            "name_filter": name_filter
+        }
+        granule09 = earthdata.search(atl09_parms)
+        if len(granule09) > 0:
+            arg = f"{granule},{granule09[0]}"
+            args_list.append(arg)
+            print(f"Appending {arg}")
+        else:
+            print(f"Skipping {granule}")
     local_cycle_file = f'data/atl03_granules_cycle_{args.cycle}.txt'
     print(f"Saving cycle {args.cycle} to file {local_cycle_file}")
     with open(local_cycle_file, 'w') as file:
-        for granule in granules:
-            file.write(f'{granule}\n')
+        for arg in args_list:
+            file.write(f'{arg}\n')
 
 # status jobs
 if args.status:
@@ -76,24 +80,29 @@ if args.status:
     print(json.dumps(jobs_in_progress, indent=2))
 
 # run results
-if args.run:
+if args.results:
     outputs = []
     s3 = boto3.client("s3", region_name="us-west-2")
     def load_remote_file(bucket, key):
         obj = s3.get_object(Bucket=bucket, Key=key)
         contents = obj["Body"].read().decode("utf-8")
         return json.loads(contents)
-    bucket = args.run.split("s3://")[-1].split("/")[0]
-    prefix = "/".join(args.run.split("s3://")[-1].split("/")[1:])
+    bucket = args.results.split("s3://")[-1].split("/")[0]
+    prefix = "/".join(args.results.split("s3://")[-1].split("/")[1:])
     results = load_remote_file(bucket, f"{prefix}/receipt.json") # {"name": ..., "username": ... "args": {"0": ..., ...}, "environment": ...}
     for item in results["args"]:
-        result = load_remote_file(bucket, f"{prefix}/result_{item}.json")
-        outputs.append(result["output"])
-        print(f"{item} =>", json.dumps(result, indent=2))
+        try:
+            result = load_remote_file(bucket, f"{prefix}/result_{item}.json")
+            output = "output" in result and result["output"] or result
+            outputs.append(output)
+            print(f"{item} =>", json.dumps(result, indent=2))
+        except Exception as e:
+            print(f"{item} =>", {e})
     print(f"Saving outputs to {args.outputs}")
     with open(args.outputs, 'w') as file:
         for output in outputs:
             file.write(f'{output}\n')
+
 
 ############################################################################################
 # session = sliderule.create_session(domain="localhost", cluster=None, verbose=True)
