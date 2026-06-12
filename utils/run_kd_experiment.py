ea1d137438dcb0c9e892ed3d254b6752cc0e5586
JP Swinski <jp.swinski@nasa.gov>
2026-06-12 13:54:30 +0000

    enhanced kd run experiment to keep track of granules

--- utils/run_kd_experiment.py
+++ utils/run_kd_experiment.py
@@ -2,71 +2,83 @@ import sys
 import boto3
 import json
 import argparse
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from sliderule import sliderule, earthdata
+from sliderule import sliderule
 
+#########################################
 # command line arguments
+#########################################
 parser = argparse.ArgumentParser(description="""Kd Experiment""")
-parser.add_argument('--name',       type=str,               default=None)
+parser.add_argument('--cycle',      type=int,               default=None) # 1, 2, 3, etc.
 parser.add_argument('--vcpus',      type=int,               default=4)
 parser.add_argument('--memory',     type=int,               default=16000)
+parser.add_argument('--batch_size', type=int,               default=10000)
 parser.add_argument('--script',     type=str,               default="utils/kd_experiment.lua")
-parser.add_argument('--arg',        type=str,               default=None) # ATL03_20241107234251_08052501_007_01.h5,ATL09_20241107234251_08052501_007_01.h5
-parser.add_argument('--args',       type=str,               default=None) # data/atl03_granules_cycle_1.txt
-parser.add_argument('--slice',      type=int, nargs=2,      default=[0, 10000])
-parser.add_argument('--submission', type=str,               default="/tmp/kd_experiment_submission.txt")
-parser.add_argument('--outputs',    type=str,               default="/tmp/kd_experiment_outputs.txt")
-parser.add_argument('--cycle',      type=int,               default=None) # 1
-parser.add_argument('--results',    action='store_true',    default=False)
-parser.add_argument('--submit',     action='store_true',    default=False)
+parser.add_argument('--database',   type=str,               default="data/kd_experiment_database.json")
 parser.add_argument('--status',     action='store_true',    default=False)
-parser.add_argument('--list_files', action='store_true',    default=False)
+parser.add_argument('--report',     action='store_true',    default=False)
 args = parser.parse_args()
 
-# create runner session and authentiate
+#########################################
+# open database
+#########################################
+# {
+#     "submissions": {
+#         "<name>": {
+#             "run_url": <run url>,
+#             "job_id": <job id>,
+#             "status": {
+#                 "SUBMITTED": <x>,
+#                 "PENDING": <x>,
+#                 "RUNNABLE": <x>,
+#                 "STARTING": <x>,
+#                 "RUNNING": <x>,
+#                 "SUCCEEDED": <x>,
+#                 "FAILED": <x>
+#             },
+#             "complete": <true|false>
+#         },
+#         ...
+#     },
+#     "granules": {
+#         "<ATL03 granule>": {
+#             "job": <name> ,-- of job responsible for processing granule
+#             "status": <"pending", "empty", "output", "error">, -- pending: has not run yet, empty: ran and produced no output, output: completed with results, error: failed to complete
+#             "output": <url to output granule>, -- e.g. s3://sliderule-public/atl24_2026...parquet
+#         }
+#     }
+# }
+try:
+    # read database
+    with open(args.database, "r") as file:
+        database = json.load(file)
+except:
+    # create database
+    with open(args.database, "w") as file:
+        database = {"submissions": {}, "granules": {}}
+        json.dump(database, file)
+
+#########################################
+# create runner sessions and authentiate
+#########################################
 session = sliderule.create_session(verbose=True)
 session.authenticate() # gives privileges to access SlideRule Runner
 
-# submit jobs
-if args.submit:
-    # check name supplied
-    if not args.name:
-        print("Error: must supply a name for the job")
-        sys.exit(1)
-    # get list of granules
-    if args.arg:
-        args_list = [args.arg]
-    elif args.args:
-        args_list = []
-        with open(args.args, "r") as file:
-            for arg in file.readlines():
-                args_list.append(arg.strip())
-    else:
-        print("Error: must supply arguments to process")
-        sys.exit(1)
-    # submit jobs to runner
-    args_list = args_list[args.slice[0]:args.slice[1]]
-    print(f"Submitting job {args.name} using script {args.script} with {len(args_list)} entries")
-    lua_script = open(args.script, "r").read()
-    rsps = session.runner.submit(name=args.name, script=lua_script, args=args_list, optional_args={"vcpus":args.vcpus, "memory":args.memory})
-    print(f"Saving job submission to {args.submission}", rsps)
-    with open(args.submission, 'w') as file:
-        file.write(f'{json.dumps(rsps, indent=2)}')
-
-# create cycle file
-if args.cycle:
+#########################################
+# function: get argument list
+#########################################
+def get_args_list(cycle):
     args_list = []
-    print(f"Requesting ATL03 granules from CMR for cycle {args.cycle}")
+    print(f"Requesting ATL03 granules from CMR for cycle {cycle}")
     atl03_granules = sliderule.source("earthdata", {
         "asset": "icesat2",
-        "cycle": args.cycle,
+        "cycle": cycle,
         "max_resources": 100000
     })
     print(f"Retrieved list of {len(atl03_granules)} granules to process")
-    print(f"Requesting ATL09 granules from CMR for cycle {args.cycle}")
+    print(f"Requesting ATL09 granules from CMR for cycle {cycle}")
     atl09_granules = sliderule.source("earthdata", {
         "asset": "icesat2-atl09",
-        "cycle": args.cycle,
+        "cycle": cycle,
         "max_resources": 100000
     })
     print(f"Retrieved list of {len(atl09_granules)} granules to process")
@@ -83,72 +95,102 @@ if args.cycle:
             args_list.append(f"{granule},{atl09_granule}")
         except Exception as e:
             print(f"Skipping granule {granule}: {e}")
-    local_cycle_file = f'data/atl03_granules_cycle_{args.cycle}.txt'
-    print(f"Saving cycle {args.cycle} to file {local_cycle_file}")
-    with open(local_cycle_file, 'w') as file:
-        for arg in args_list:
-            file.write(f'{arg}\n')
+    return args_list
 
-# status last job submitted
-if args.status:
-    # get progress of submitted jobs
-    with open(args.submission) as file:
-        submission = json.loads(file.read())
-    jobs_in_progress = session.runner.queue(name=submission["name"], job_id=submission["job_id"])
-    print(json.dumps(jobs_in_progress["report"], indent=2))
-
-# run results
-if args.results:
-    outputs = []
-    with open(args.submission) as file:
-        submission = json.loads(file.read())
+#########################################
+# function: get results
+#########################################
+def get_results(run_url):
+    results = {}
     s3 = boto3.client("s3", region_name="us-west-2")
     def load_remote_file(bucket, key):
         obj = s3.get_object(Bucket=bucket, Key=key)
         contents = obj["Body"].read().decode("utf-8")
         return json.loads(contents)
-    bucket = submission["run_url"].split("s3://")[-1].split("/")[0]
-    prefix = "/".join(submission["run_url"].split("s3://")[-1].split("/")[1:])
+    bucket = run_url.split("s3://")[-1].split("/")[0]
+    prefix = "/".join(run_url.split("s3://")[-1].split("/")[1:])
     results = load_remote_file(bucket, f"{prefix}/receipt.json") # {"name": ..., "username": ... "args": <path to arg file>, "environment": ...}
     args_list = load_remote_file(bucket, results["args"])
     for i in range(len(args_list)):
+        granule,_ = args_list[i].split(",")
         try:
-            result = load_remote_file(bucket, f"{prefix}/result{i}.json")
-            output = "output" in result and result["output"] or result
-            outputs.append(output)
-            print(f"{args_list[i]} =>", json.dumps(result, indent=2))
+            result = {}
+            rsps = load_remote_file(bucket, f"{prefix}/result{i}.json")
+            if not rsps["status"] or "output" not in rsps:
+                result["status"] = "empty"
+            else:
+                result["status"] = "output"
+                result["output"] = rsps["output"]
+                result["duration"] = rsps["stop"] - rsps["start"]
+            results[granule] = result
         except Exception as e:
-            print(f"{i} =>", {e})
-    print(f"Saving outputs to {args.outputs}")
-    with open(args.outputs, 'w') as file:
-        for output in outputs:
-            file.write(f'{output}\n')
+            results[granule] = {"status": "error"}
+    return results
 
-# List Files in Bucket
-if args.list_files:
-    data_files = {}
-    s3 = boto3.client("s3")
-    is_truncated = True
-    continuation_token = None
-    while is_truncated:
-        # make request
-        if continuation_token:
-            response = s3.list_objects_v2(Bucket="sliderule-public", Prefix="", ContinuationToken=continuation_token)
-        else:
-            response = s3.list_objects_v2(Bucket="sliderule-public", Prefix="")
-        # display status
-        sys.stdout.write("#")
-        sys.stdout.flush()
-        # parse contents
-        if 'Contents' in response:
-            for obj in response['Contents']:
-                resource = obj['Key'].split("/")[-1]
-                if resource.endswith(".kd.v4.parquet"):
-                    data_files[resource] = obj['Size']
-        # check if more data is available
-        is_truncated = response['IsTruncated']
-        continuation_token = response.get('NextContinuationToken')
-    print(f"\nWriting {len(data_files)} files names to {args.outputs}")
-    with open(args.outputs, "w") as file:
-        for data_file in data_files:
-            file.write(f"{data_file}\n")
+#########################################
+# process granules for cycle
+#########################################
+if args.cycle:
+
+    # get all arguments for the cycle
+    args_for_cycle = get_args_list(args.cycle)
+    for i in range(0, len(args_for_cycle), args.batch_size):
+
+        # submit job
+        name = f"kd{args.cycle}_{i}"
+        args_list = args_for_cycle[i:i+args.batch_size]
+        lua_script = open(args.script, "r").read()
+        rsps = session.runner.submit(name=name, script=lua_script, args=args_list, optional_args={"vcpus":args.vcpus, "memory":args.memory})
+        print(f"Submitted job {name} using script {args.script} with {len(args_list)} entries")
+
+        # save job
+        database["submissions"][name] = rsps | {"completed": False}
+        print(f"Saved job submission", rsps)
+
+        # save granules
+        for granules in args_list:
+            atl03_granule, atl09_granule = granules.split(",")
+            database["granules"] = {"name": name, "status": "pending"}
+
+#########################################
+# status jobs
+#########################################
+if args.status:
+
+    # get status
+    for name,job in database["submissions"].items():
+        if not job["complete"]:
+            status = session.runner.queue(name=name, job_id=job["job_id"])["report"]
+            database["submissions"][name]["status"] = status
+            if sum([status[s] for s in ["SUBMITTED", "PENDING", "RUNNABLE", "STARTING", "RUNNING"]]) == 0:
+                database["submissions"]["name"]["complete"] = True
+                results = get_results(job["run_url"])
+                for granule, result in results.items():
+                    database["granules"][granule] |= result
+
+    # display status
+    columns = ["SUCCEEDED", "FAILED", "RUNNING", "STARTING", "RUNNABLE", "PENDING", "SUBMITTED"]
+    print(",".join([f"{c:>10}" for c in ["NAME"] + columns]))
+    for name,job in database["submissions"].items():
+        print(",".join([f"{c:>10}" for c in [name] + [job["status"][state] for state in columns]]))
+
+#########################################
+# report on status of granules
+#########################################
+if args.report:
+
+    stats = {
+        "pending": 0,
+        "empty": 0,
+        "output": 0,
+        "error": 0
+    }
+    for granule,data in database["granules"].items():
+        stats[data["status"]] += 1
+    print(json.dumps(stats, indent=2))
+
+#########################################
+# save database
+#########################################
+with open(args.database, 'w') as file:
+    json.dump(database, file)
