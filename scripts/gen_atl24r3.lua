df085793ebf22084285a8cc7aae94a13a5ccfeb9
JP Swinski <jp.swinski@nasa.gov>
2026-09-21 20:17:03 +0000

    increased timeout to 3 hours

--- scripts/gen_atl24r3.lua
+++ scripts/gen_atl24r3.lua
@@ -3,7 +3,7 @@ local json                  = require("json")
 local aws_utils             = require("aws_utils")
 local version, _, build, _  = sys.version()
 local release               = "3"
-local timeout               = 5400 * 1000
+local timeout               = 10800 * 1000 -- 3 hours
 local result                = { status = true, build = build, start = time.latch(), messages = {} }
 local consoleq              = msg.subscribe("consoleq") -- prevents error posting to consoleq
 
